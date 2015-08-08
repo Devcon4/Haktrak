35fb6231a68382b67237e5b0b39d468a5be818bb
Devyn Cyphers <devcon4444@gmail.com>
2015-08-08 17:34:12 -0600

    Basic UX implimentation
    added basic routing, templating, and site UI.
    
    Author: Devyn Cyphers; Devcon

--- Haktrak/Haktrak.js
+++ Haktrak/Haktrak.js
@@ -1,23 +1,52 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+// ID of the Google Spreadsheet
+ var spreadsheetID = "1hFsBm010k5GqV-4VfW4pEZLqARV-dyLbwnbBUQR1h1Q";
+ 
+ // Make sure it is public or set to Anyone with link can view 
+ var url = "https://spreadsheets.google.com/feeds/list/"+spreadsheetID+"/od6/public/values?alt=json";
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+//questLog = new Mongo.Collection("quests");
+var jsonDep = new Deps.Dependency();
+var jsonAr = [];
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+if (Meteor.isClient) {  
+  Template.quests.helpers({
+    lines: function(){ 
+      getData();
+      jsonDep.depend();
+      return jsonAr;
     }
   });
 }
 
+function getData(){
+  $.getJSON(url, function(data) {
+    for(var i = 0; i < data.feed.entry.length; i++){
+      jsonAr[i] = {value: data.feed.entry[i].gsx$value.$t, text: data.feed.entry[i].gsx$text.$t};
+    }
+    jsonDep.changed();
+  });
+}
+            
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
 }
+
+//Routes
+Router.route('/', {
+  name: 'home',
+  template: 'home'
+});
+Router.route('/quests', {
+  name: 'quests',
+  tmeplate: 'quests'
+});
+Router.route('/stats', {
+  name: 'stats',
+  template: 'stats'
+});
+
+Router.configure({
+  layoutTemplate: 'layout1'
+});
